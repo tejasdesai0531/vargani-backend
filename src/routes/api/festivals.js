3cada61128831689a6670819b2bba034a8773676
desaitejas <tejas@mutterfly.in>
2021-01-03 19:00:09 +0530

    Adds error

--- src/routes/api/festivals.js
+++ src/routes/api/festivals.js
@@ -43,6 +43,7 @@ route.post('/editFestival', async(req, res) => {
 })
 
 route.get('/getAllFestivals', async(req, res) => {
+    console.log(hii)
     try {
         const result = await getAllFestivals()
         
